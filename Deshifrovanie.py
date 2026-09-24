337d51d7bf3c112fa3b0efbadb6477e1ec24e0df
rus1117 <ruslanparhadov@gmail.com>
2026-09-24 21:03:50 +0500

    Add files via upload

---
+++ Deshifrovanie.py
@@ -0,0 +1,27 @@
+text = input("Введите текст для расшифровки: ")
+shift = int(input("Введите величину сдвига: ")) #если при шифрованиии вы ввели 5 то для расшифровки введите так же 5
+
+
+result = []
+for char in text:
+    if 'a' <= char <= 'z':
+        start = ord('a')
+        result.append(chr((ord(char) - start - shift) % 26 + start))
+    elif 'A' <= char <= 'Z':
+        start = ord('A')
+        result.append(chr((ord(char) - start - shift) % 26 + start))
+    elif 'а' <= char <= 'я':
+        start = ord('а')
+        result.append(chr((ord(char) - start - shift) % 32 + start))
+    elif 'А' <= char <= 'Я':
+        start = ord('А')
+        result.append(chr((ord(char) - start - shift) % 32 + start))
+    elif char in 'ёЁ':
+        base = 'е' if char == 'ё' else 'Е'
+        start = ord(base)
+        result.append(chr((ord(base) - start - shift) % 32 + start))
+    else:
+        result.append(char)
+
+decrypted_text = "".join(result)
+print(f"Расшифрованный текст: {decrypted_text}")
